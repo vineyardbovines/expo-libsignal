e8bd529947b329af5717a352c3dc11610913029b
vineyardbovines <spencerfpope@gmail.com>
2026-06-05 11:35:20 -0400

    feat(android): wire IdentityKeyPair into module definition
    Registers IdentityKeyPairRef as an Expo Class with generate/deserialize
    (async, coroutine-backed) and serialize/publicKey/privateKey (sync), and
    exposes serialize() on PublicIdentityKeyRef and PrivateKeyRef. Uses
    IdentityKeyPair.generate() static directly instead of the now-removed
    Curve API.

--- android/src/main/java/expo/modules/libsignal/ExpoLibsignalModule.kt
+++ android/src/main/java/expo/modules/libsignal/ExpoLibsignalModule.kt
@@ -1,10 +1,50 @@
 package expo.modules.libsignal
 
+import expo.modules.kotlin.functions.Coroutine
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import org.signal.libsignal.protocol.IdentityKeyPair as SignalIdentityKeyPair
 
 class ExpoLibsignalModule : Module() {
   override fun definition() = ModuleDefinition {
     Name("ExpoLibsignal")
+
+    Class(IdentityKeyPairRef::class) {
+      AsyncFunction("generate") Coroutine { ->
+        IdentityKeyPairRef(SignalIdentityKeyPair.generate())
+      }
+
+      AsyncFunction("deserialize") Coroutine { bytes: ByteArray ->
+        try {
+          IdentityKeyPairRef(SignalIdentityKeyPair(bytes))
+        } catch (e: Throwable) {
+          throw RuntimeException(mapSignalError(e).message)
+        }
+      }
+
+      Function("serialize") { ref: IdentityKeyPairRef ->
+        ref.keyPair.serialize()
+      }
+
+      Function("publicKey") { ref: IdentityKeyPairRef ->
+        PublicIdentityKeyRef(ref.keyPair.publicKey)
+      }
+
+      Function("privateKey") { ref: IdentityKeyPairRef ->
+        PrivateKeyRef(ref.keyPair.privateKey)
+      }
+    }
+
+    Class(PublicIdentityKeyRef::class) {
+      Function("serialize") { ref: PublicIdentityKeyRef ->
+        ref.key.serialize()
+      }
+    }
+
+    Class(PrivateKeyRef::class) {
+      Function("serialize") { ref: PrivateKeyRef ->
+        ref.key.serialize()
+      }
+    }
   }
 }
